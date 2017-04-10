c166cfc05c317b4da9b8b5fad6037bb8bc609aa2
dev-qiukai <qiukai@letv.com>
2017-04-10 18:07:50 +0800

    update

---
+++ TestView.h
@@ -0,0 +1,15 @@
+//
+//  TestView.h
+//  HidingNavigationBarSample
+//
+//  Created by qiukai on 17/4/10.
+//  Copyright © 2017年 Tristan Himmelman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+- (void)test;
+
+@end
---
+++ TestView.m
@@ -0,0 +1,18 @@
+//
+//  TestView.m
+//  HidingNavigationBarSample
+//
+//  Created by qiukai on 17/4/10.
+//  Copyright © 2017年 Tristan Himmelman. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+- (void)test
+{
+    NSLog(@"========test====test====");
+}
+
+@end
