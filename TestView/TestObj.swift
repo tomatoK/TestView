3ed9ce241a5029b844e4cba8e802f6e9b07cce93
dev-qiukai <qiukai@letv.com>
2017-04-13 14:39:44 +0800

    update

---
+++ TestView/TestObj.swift
@@ -0,0 +1,15 @@
+//
+//  TestObj.swift
+//  Albatross
+//
+//  Created by qiukai on 17/4/13.
+//  Copyright © 2017年 LeSports Inc. All rights reserved.
+//
+
+import UIKit
+
+class TestObj: NSObject {
+    func speak() {
+        print("========speak==speak=")
+    }
+}
